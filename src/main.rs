5785f41cd18c2a62c7fad22e8525cbc2af7fb17c
Benjamin Aaron <ben@theaarons.com>
2024-11-09 20:52:57 -0800

    implement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let random_string: String = (0..7)
+        .map(|_| {
+            let mut rng = rand::thread_rng();
+            let char_code = rng.gen_range(65..=90);
+            char::from_u32(char_code).unwrap()
+        })
+        .collect();
+
+    println!("{}", random_string);
 }
