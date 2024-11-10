7d91d486f46881d3da4b893d216b03289edd446a
Benjamin Aaron <ben@theaarons.com>
2024-11-09 21:11:01 -0800

    loop it

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
+use clipboard::{ClipboardContext, ClipboardProvider};
 use rand::Rng;
+use std::{thread, time::Duration};
 
-fn main() {
-    let random_string: String = (0..7)
-        .map(|_| {
-            let mut rng = rand::thread_rng();
-            let char_code = rng.gen_range(65..=90);
-            char::from_u32(char_code).unwrap()
-        })
-        .collect();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut ctx: ClipboardContext = ClipboardProvider::new()?;
+    let mut rng = rand::thread_rng();
 
-    print!("{}", random_string);
+    println!("Generating random strings to clipboard. Press Ctrl+C to stop...");
+
+    loop {
+        let random_string: String = (0..7)
+            .map(|_| {
+                let char_code = rng.gen_range(65..=90);
+                char::from_u32(char_code).unwrap()
+            })
+            .collect();
+
+        ctx.set_contents(random_string.clone())?;
+        println!("Copied to clipboard: {}", random_string);
+
+        thread::sleep(Duration::from_secs(1));
+    }
 }
